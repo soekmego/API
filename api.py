aa1d97ccf0f6b114dc8f8c7bc9500a925b49722f
soekmego <soekmen.goekhan@gmail.com>
2017-06-17 18:07:25 +0200

    figuring out the order of the task

---
+++ api.py
@@ -0,0 +1,25 @@
+"""
+API learning project. 
+
+We want to learn how to grab raw data from publicly
+availabe data by accesing it via API, parse the data and make 
+a graph with help of matplotlib
+"""
+
+def main():
+    """This function handles the logic of this script"""
+
+    #Grab CPI/inflation data
+
+    #Grab API/game platform data
+
+    #Figure out the current price of each platform.
+    #This will require looping through each game platform we received,
+    #and calculate the adjusted price based on the CPI data we also
+    #received.
+    #During this point we also validate our data so we do not skew
+    #our results.
+
+    #Generate a plot/bar graph for adjusted price data.
+
+    #Generate a CSV file to save for the adjusted price data.
