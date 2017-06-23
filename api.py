0cb7c027066202a5460d4b073016d80c9094f82c
soekmego <soekmen.goekhan@gmail.com>
2017-06-23 15:10:32 +0200

    added price adjustment logic

--- api.py
+++ api.py
@@ -64,12 +64,62 @@ class CPIData(object):
 
     def load_from_file(self, fp):
         """Loads CPI data from a given file-like object"""
+        current_year = None
+        year_cpi = []
+
+        for line in fp:
+            #The actual content of the file starts with a header line
+            #starting with the string "DATE". Until we reach this line,
+            #we can skip ahead
+            while not line.startswith("DATE"):
+                pass
+        #Each line ends with a new-line character which we strip here
+        #to make the data usable easier.
+        data = line.rstrip().split()
+
+        #While we are dealing with calendar data, the format is simlpe
+        #enough that we dont need a full date parser. All we want is
+        #the year which can be extracted by simple string splitting:
+        year = int(data[0].split("-")[0])
+        cpi = float(data[1])
+
+        if self.first_year is None:
+            self.first_year = year
+        self.last_year = year
+
+        #The moment we reach a new year, we have to reset the CPI data
+        #and calculate the average CPI of the current_year.
+        if current_year != year:
+            if current_year is not None:
+                self.year_cpi[current_year] = sum(year_cpi) / len(year_cpi)
+            year_cpi = []
+            current_year = year
+        year_cpi.append(cpi)
+
+        #We have to do the calculation once again for the last year
+        #in the dataset.
+        if current_year is not None and current_year not in self.year_cpi:
+            self.year_cpi[current_year] = sum(year_cpi) / len(year_cpi)
 
     def get_adjusted_price(self, price, year, current_year=None):
         """Returns the adapted price from a given year compared to what
         current year has been specified.
 
         """
+        #Currently, there is no dataset for 2014
+        if current_year is None or current_year > 2013:
+            current_year = 2013
+        #If our datarange doesnt provide a CPI for the given year,
+        #use the edge data.
+        if year < self.first_year:
+            year = self.first_year
+        elif year > self.last_year:
+            year = self.last_year
+
+        year_cpi = self.year_cpi[year]
+        current_cpi = self.year_cpi[current_year]
+
+        return float(price) / year_cpi * current_cpi
 
 def main():
     """This function handles the logic of this script"""
