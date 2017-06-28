0b7667e2a80d9a3b99efc770ad8feaa00d5de455
soekmego <soekmen.goekhan@gmail.com>
2017-06-28 21:31:00 +0200

    generate plot

--- api.py
+++ api.py
@@ -9,6 +9,9 @@ a graph with help of matplotlib
 from __future__ import print_function
 import requests
 import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import tablib
 
 CPI_DATA_URL = "http://research.stlouisfed.org/fred2/data/CPIAUCSL.txt"
 
@@ -208,6 +211,86 @@ class GiantbombAPI(object):
 
                 yield item
                 counter += 1
+
+def generate_plot(platforms, output_file):
+    """Generates a bar chart out of the given platforms and writes the output
+    into the specified file as PNG image.
+
+    """
+    # First off we need to convert the platforms in a format that can be
+    # attached to the 2 axis of our bar chart. "labels" will become the
+    # x-axis and "values" the value of each label on the y-axis:
+    labels = []
+    values = []
+    for platform in platforms:
+        name = platform["name"]
+        adapted_price = platform["adjusted_price"]
+        price = platform["original_price"]
+
+        # Skip prices higher than 2000 USD simply because it would make the
+        # output unusable.
+        if price > 2000:
+            continue
+
+        # If the name of the platform is too long, replace it with the
+        # abbreviation. list.insert(0, val) inserts val at the beginning of
+        # the list.
+        if len(name) > 15:
+            name = platform["abbreviation"]
+        labels.insert(0, u"{0}\n$ {1}\n$ {2}".format(name, price,
+                                                    round(adjusted_price, 2)))
+        values.insert(0, adjusted_price)
+
+    # Let's define the width of each bar and the size of the resulting graph.
+    width = 0.3
+    ind = np.arange(len(values))
+    fig = plt.figure(figsize=(len(labels) * 1.8, 10))
+
+    # Generate a subplot and put our values onto it.
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(ind, values, width, align="center")
+
+    # Format the X and Y axis labels. Also set the ticks on the x-axis slightly
+    # farther apart and give then a slight tilting effect.
+    plt.ylabel("Adjusted price")
+    plt.xlabel("Year / Console")
+    ax.set_xticks(ind + 0.3)
+    ax.set_xticklabels(labels)
+    fig.autofmt_xdate()
+    plt.grid(True)
+
+    plt.savefig(output_file, dpi=72)
+    plt.show(dpi=72)
+
+def generate_csv(platforms, output_file):
+    """Writes the given platforms into a CSV file specified by the output_file
+    parameter.
+
+    The output_file can either be the path to a file or a file-like object.
+    
+    """
+    ### TODO ###
+    ### ### ###
+
+def is_valid_dataset(platform):
+    """Filters out datasets that we can't use since they are either lacking
+    a release date or an original price. For rendering the output we also
+    require the name and abbreviation of the platform.
+
+    """
+    if "release_date" not in platform or not platform["release_date"]:
+        logging.warn(u"{0} has no release date".format(platform["name"]))
+        return False
+    if "original_price" not in platform or not platform["original_price"]:
+        logging.warn(u"{0} has no original price".format(platform["name"]))
+        return False
+    if "name" not in platform or not platform["name"]:
+        logging.warn(u"No platform name found for given dataset")
+        return False
+    if "abbreviation" not in platform or not platform["abbreviation"]:
+        logging.warn(u"{0} has no abbreviation".format(platform["name"]))
+        return False
+    return True
 
 
 def main():
